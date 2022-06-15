e10e64fe9004d6f9e5557554fdf9eba8946dd277
yu5uf01 <107554140+yu5uf01@users.noreply.github.com>
2022-06-15 19:26:26 +0700

    Add files via upload

---
+++ muhammadyusuf.py
@@ -0,0 +1,42 @@
+print ("________PROGRAM JUAL BELI_______")
+
+print ("=======MENU PILIHAN=======")
+print ("1. PISCOK MINI        : Rp 15,000")
+print ("2. PISCOK LUMER    : Rp 18,000")
+print ("3. PISCOK JUMBO  :Rp 20,000")
+
+pilih = int(input("====pilih menu==== :"))
+
+if pilih == 1:
+    print("piscok mini seharga seharga Rp. 15,000")
+    jumlah = int(input("jumlah pesanan ="))
+    print("")
+    total = jumlah*15000
+    print("total harga",total)
+    print("====mator sakalangkong=====")
+elif pilih == 2:
+    print("piscok lumer seharga seharga Rp. 18,000")
+    jumlah = int(input("jumlah pesanan ="))
+    print("")
+    total = jumlah*18000
+    print("total harga",total)
+    print("====mator sakalangkong=====")
+elif pilih == 3:
+    print("piscok jumbo  seharga Rp. 20,000")
+    jumlah = int(input("jumlah pesanan ="))
+    print("")
+    total = jumlah*20000
+    print("total harga",total)
+    print("====mator sakalangkong=====")
+else:
+    print(",,,,,,,baca basmalah,,,,,,,")
+    print("jangan lupa yyyyyyyyyyyy")
+
+class pilihan:
+	def __init__(self, pilihan):
+		self.pillihan = pilihan
+
+class pilihan:
+	pass 
+	('pilihan')
+print('pilihan anda telah di akses')
